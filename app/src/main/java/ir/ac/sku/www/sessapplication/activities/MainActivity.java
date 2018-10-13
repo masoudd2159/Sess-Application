64b2bd8cd1912e96d2a47744002cccce2dd7a4a9
Masoud Dabbaghi <masoudd2159@gmail.com>
2018-10-13 21:09:53 +0330

    write class splash screen

---
+++ app/src/main/java/ir/ac/sku/www/sessapplication/activities/MainActivity.java
@@ -0,0 +1,15 @@
+package ir.ac.sku.www.sessapplication.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import ir.ac.sku.www.sessapplication.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
--- app/src/main/java/ir/ac/sku/www/sessapplication/activities/SplashScreenActivity.java
+++ app/src/main/java/ir/ac/sku/www/sessapplication/activities/SplashScreenActivity.java
@@ -1,15 +1,62 @@
 package ir.ac.sku.www.sessapplication.activities;
 
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.AsyncTask;
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 
 import ir.ac.sku.www.sessapplication.R;
 
 public class SplashScreenActivity extends AppCompatActivity {
 
+    private static final int SPLASH_TIME = 10000;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            Window window = getWindow();
+            window.getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            window.setStatusBarColor(Color.TRANSPARENT);
+        }
+
+        new BackgroundTask().execute();
     }
+
+    private class BackgroundTask extends AsyncTask {
+
+        Intent intent;
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            intent = new Intent(SplashScreenActivity.this, MainActivity.class);
+        }
+
+        @Override
+        protected Object doInBackground(Object[] objects) {
+            try {
+                Thread.sleep(SPLASH_TIME);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Object o) {
+            super.onPostExecute(o);
+            startActivity(intent);
+            finish();
+        }
+    }
+
 }
