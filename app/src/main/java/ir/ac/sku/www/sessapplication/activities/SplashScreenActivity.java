13c8755457499dcb02cf2a67b5334fa80dfd931e
Masoud Dabbaghi <masoudd2159@gmail.com>
2018-10-13 19:46:56 +0330

    design splash screen

---
+++ app/src/main/java/ir/ac/sku/www/sessapplication/activities/SplashScreenActivity.java
@@ -0,0 +1,15 @@
+package ir.ac.sku.www.sessapplication.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import ir.ac.sku.www.sessapplication.R;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+    }
+}
